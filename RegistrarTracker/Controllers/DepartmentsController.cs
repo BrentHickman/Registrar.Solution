b7da015015909e68df45dd74bc5fe4eff05620ba
xayananh4 <xayananh4@gmail.com>
2023-03-15 16:22:23 -0700

    WIP adding departments functionality
    Co-Authored-by: Brent Hickman <brenthickman@ymail.com>

--- RegistrarTracker/Controllers/DepartmentsController.cs
+++ RegistrarTracker/Controllers/DepartmentsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using RegistrarTracker.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,10 +38,28 @@ namespace RegistrarTracker.Controllers
     {
       Department thisDepartment = _db.Departments
                                 .Include(department => department.Students)
-                                .ThenInclude(item => item.JoinEntities)
+                                .ThenInclude(student => student.JoinEntities)
                                 .ThenInclude(join => join.Course)
                                 .FirstOrDefault(department => department.DepartmentId == id);
       return View(thisDepartment);
     }
+    public ActionResult AddStudent(int id)
+    {
+      Department thisDepartment = _db.Departments.FirstOrDefault(departments => departments.DepartmentId == id);
+      ViewBag.StudentId = new SelectList(_db.Students, "StudentId", "StudentName");
+      return View(thisDepartment);
+    }
+    // [HttpPost]
+    // public ActionResult AddStudent(Student student)
+    // {
+    //   if (student.StudentId == 0)
+    //   {
+    //     return RedirectToAction("Create", "Students");
+    //   }
+      
+    //   _db.Students.Update(student.DepartmentId);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
   }
 }
